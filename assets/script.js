e54816cbede66cf74c16a2b8a723d550a8bfe627
Abed Hasan <abedhasan79@gmail.com>
2022-09-27 20:25:03 -0400

    fixed local storage issue where it would only save the 1st time block

--- assets/script.js
+++ assets/script.js
@@ -34,28 +34,69 @@ setInterval(function () {
         }
     }
 
+
+
 }, 1000);
 
 
 
 //saves inputs inside text area in local storage
-for (let i = 9; i < 18; i++) {
-    $(".saveBtn"+i.toString()).on('click', function () {
-
-        // console.log($('#'+i.toString()).val());
-        localStorage.setItem("timerBlock"+i.toString(), ($('#'+i.toString()).val()));
-
+function saveToStorage() {
+    $(".saveBtn9").on('click', function () {
+        localStorage.setItem("timerBlock9", $('#9').val());
     });
+
+    $(".saveBtn10").on('click', function () {
+        localStorage.setItem("timerBlock10", $('#10').val());
+    });
+
+    $(".saveBtn11").on('click', function () {
+        localStorage.setItem("timerBlock11", $('#11').val());
+    });
+
+    $(".saveBtn12").on('click', function () {
+        localStorage.setItem("timerBlock12", $('#12').val());
+    });
+
+    $(".saveBtn13").on('click', function () {
+        localStorage.setItem("timerBlock1", $('#13').val());
+    });
+
+    $(".saveBtn14").on('click', function () {
+        localStorage.setItem("timerBlock2", $('#14').val());
+    });
+
+    $(".saveBtn15").on('click', function () {
+        localStorage.setItem("timerBlock3", $('#15').val());
+    });
+
+    $(".saveBtn16").on('click', function () {
+        localStorage.setItem("timerBlock4", $('#16').val());
+    });
+
+    $(".saveBtn17").on('click', function () {
+        localStorage.setItem("timerBlock5", $('#17').val());
+    });
+
 }
 
+saveToStorage()
 
 // gets save info for text area from local storage and set the text in text area when page if refreshed
-for (let j = 9; j < 18; j++) {
-
-    // console.log("timerBlock" + j.toString());
-    $('#'+j.toString()).text((localStorage.getItem(("timerBlock"+j.toString()))));
-
+function renderMessage() {
+    $('#9').text((localStorage.getItem("timerBlock9")));
+    $('#10').text((localStorage.getItem("timerBlock10")));
+    $('#11').text((localStorage.getItem("timerBlock11")));
+    $('#12').text((localStorage.getItem("timerBlock12")));
+    $('#13').text((localStorage.getItem("timerBlock1")));
+    $('#14').text((localStorage.getItem("timerBlock2")));
+    $('#15').text((localStorage.getItem("timerBlock3")));
+    $('#16').text((localStorage.getItem("timerBlock4")));
+    $('#17').text((localStorage.getItem("timerBlock5")));
 }
+
+renderMessage();
+
 
 
 
